37ef06c1b5abf0befc751ad17ad70bf5325b0a8b
Carlos Huerta <c.huerta.santana@gmail.com>
2017-11-26 14:05:09 -0300

    first commit

---
+++ BookService-master/BookService/Filters/LogFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace IdalenBackendServices.Filters
+{
+    public class LogFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            base.OnActionExecuting(actionContext);
+            DateTime dateTime = DateTime.Now;
+            try
+            {
+                Logs.EventLoghelper.WriteBodyLog("InitialTimestamp: " + dateTime.ToString() + "\n");
+            }
+            catch (Exception e) {
+                String msg = e.Message;
+            }           
+
+        }
+    }
+}
---
+++ BookService-master/BookService/Logs/EventLoghelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace IdalenBackendServices.Logs
+{
+    public class EventLoghelper
+    {
+        static readonly object _fileAccess = new object();
+
+        public static void WriteBodyLog(string msg)
+        {
+            lock (_fileAccess) {
+                string head = msg;
+                var path = System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/LogFile.txt");
+                // Write the string to a file.
+
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(head);
+                }
+            }
+            
+
+        }
+       
+    }
+}
